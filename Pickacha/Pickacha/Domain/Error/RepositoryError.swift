df237db6782888f3642ebe213e4d5820ae5363b1
J <knowisdom314@gmail.com>
2025-06-08 00:26:17 +0900

    [Feat] 에러처리 Enum 생성

---
+++ Pickacha/Pickacha/Domain/Error/RepositoryError.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryError.swift
+//  Pickacha
+//
+//  Created by J on 6/7/25.
+//
+
+import Foundation
+
+enum RepositoryError: Error {
+    case notFound
+    case saveFailed
+}
---
+++ Pickacha/Pickacha/Domain/Error/TournamentUseCaseError.swift
@@ -0,0 +1,12 @@
+//
+//  TournamentUseCaseError.swift
+//  Pickacha
+//
+//  Created by J on 6/7/25.
+//
+
+import Foundation
+
+enum TournamentUseCaseError: Error {
+    case historyNotStarted
+}
