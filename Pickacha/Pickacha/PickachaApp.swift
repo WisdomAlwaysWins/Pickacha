fa577e4eb574305c15af18e55d7c48eb7809676d
J <knowisdom314@gmail.com>
2025-06-07 18:34:11 +0900

    [Setting] 기초 레포지토리 세팅

---
+++ Pickacha/Pickacha/PickachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickachaApp.swift
+//  Pickacha
+//
+//  Created by J on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PickachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
